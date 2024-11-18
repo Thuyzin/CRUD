2409c6a40525acc703fbba978760c78bdcf760da
Arthur Affonso <arthuraffonsodejesus15@gmail.com>
2024-11-18 20:05:22 -0300

    CRUD - FEITO CREATE E READ

---
+++ src/main/java/br/senac/sp/produto/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package br.senac.sp.produto.repository;
+
+import br.senac.sp.produto.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
